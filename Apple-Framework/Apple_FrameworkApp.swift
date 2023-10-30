42c31bbb2c5b3ea4cb89048f59a7a2ca8003ad59
tauseefnasir <tauseefnasir501@gmail.com>
2023-10-30 22:45:13 +0500

    Initial Commit

---
+++ Apple-Framework/Apple_FrameworkApp.swift
@@ -0,0 +1,17 @@
+//
+//  Apple_FrameworkApp.swift
+//  Apple-Framework
+//
+//  Created by Nasir on 30/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct Apple_FrameworkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
